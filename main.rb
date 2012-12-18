c932291b399661a007a3660d8243f73e4ac4a2dd
grzegorz9 <grzegorz.muszynski9@gmail.com>
2012-12-18 17:43:54 +0000

    Welcome Screen

---
+++ main.rb
@@ -0,0 +1,15 @@
+def welcome
+  puts "----------\nWelcome to the Number Guessing Game!\n----------"
+end
+
+def main_screen
+  puts "Here's what you can do:\n"
+  puts "-- 'game' to start a new game"
+  puts "-- 'records' to display the players' with the highests results"
+  puts "-- 'options' to tweak the game"
+  puts "-- 'quit' to close the game"  
+end
+
+def process(command)
+  
+end
