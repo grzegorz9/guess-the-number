545a6c170dc81a6109dcfb95816519af5a2b6ecf
grzegorz9 <grzegorz.muszynski9@gmail.com>
2012-12-18 17:51:16 +0000

    Basic Commands Processing

--- main.rb
+++ main.rb
@@ -7,9 +7,18 @@ def main_screen
   puts "-- 'game' to start a new game"
   puts "-- 'records' to display the players' with the highests results"
   puts "-- 'options' to tweak the game"
+  puts "-- 'help' to display the manual"
   puts "-- 'quit' to close the game"  
 end
 
-def process(command)
-  
+def process(cmnd)
+  if cmnd = "game"
+    puts "Started a new game"
+  elsif cmnd = "records"
+    puts "Display the records from a file"
+  elsif cmnd = "options"
+    puts "Display the options screen"
+  elsif cmnd = "help"
+    puts "Display the manual from the file"
+  end
 end
